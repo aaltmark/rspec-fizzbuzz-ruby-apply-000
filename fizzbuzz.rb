cb8a0d46fab92404cbbbbfa067cc2b150783c25a
Adam Altmark <adamaltmark@gmail.com>
2020-04-20 18:18:18 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,11 @@
-def fizzbuzz
+def fizzbuzz(int)
+  if int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
+  elsif int % 3 == 0 && int % 5 == 0
+    "Fizzbuzz"
+  elsif int !% 3 == 0 && !% int % 5 == 0
+    "nil"
+  end
 end
